07d67e129983371b2675ea0d07a18ea4e2da3948
SHAHID AHAMED <102601195+Shahid-1102@users.noreply.github.com>
2024-12-07 04:19:45 +0530

    Fixed Cors Origin error added config file

--- MovieClient/movie-site-v1/src/api/axiosConfig.js
+++ MovieClient/movie-site-v1/src/api/axiosConfig.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export default axios.create({
     //baseURL:'https://f836-2401-4900-889f-af7b-2116-2e2a-9502-99aa.ngrok-free.app',
-    baseURL:"http://localhost:8080",
+    baseURL:`${process.env.REACT_APP_API_URL}`,
     headers: {"ngrok-skip-browser-warning": "true"}
 })
